73d0f3ca9c1f6c43343cc39b7e8e3aa7ed3dd706
RisingOrange <31575114+RisingOrange@users.noreply.github.com>
2022-08-17 11:30:08 +0200

    ref: remove not needed line

--- deduplicate_pdf.py
+++ deduplicate_pdf.py
@@ -12,7 +12,6 @@ doc = fitz.open(args.input_file)
 duplicates = []
 prev_page = doc[0] if len(doc) else None  # dont throw exception if empty
 for i, page in enumerate(list(doc)[1:], 1):
-    text_dict = page.get_text("dict")
     if page.get_text("text") == prev_page.get_text("text"):
         duplicates.append(i - 1)
     prev_page = page
